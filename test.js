bd438c2d1344ce11c1078bbf3d8e330149f477a4
Jason <22659287+Jason1im@users.noreply.github.com>
2019-10-22 18:28:45 +0800

    added logging.js and code to enable bootstrap

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-
 var x = "hello";
 
 console.log(x);
+
+document.dispatchEvent(new CustomEvent('log', {
+    detail: {
+        eventName: 'myevent',
+        info: { key1: val1, key2: val2 }
+    }
+}));
