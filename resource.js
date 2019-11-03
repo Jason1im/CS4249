28a8699994b9ea94390b7c1f1aaff5c69da2eed2
Jason <22659287+Jason1im@users.noreply.github.com>
2019-11-03 23:37:36 +0800

    allow rooms to be shown when either type and location is checked

--- resource.js
+++ resource.js
@@ -1,33 +1,41 @@
-$('#type-checkbox-list :checkbox').click(function (e) {
+var locationchecked = false;
+var typechecked = false;
+
+$('#type-checkbox-list :checkbox').click(function (eventObj) {
     $("#type-checkbox-list :checkbox").each(function(){
-        if ($(this).prop('id') != e.target.id) {
+        if ($(this).prop('id') != eventObj.target.id) {
             $(this).prop('checked', false);
+        } else {
+            typechecked = $(this).prop('checked');
         }
     })
-    displayOptions('#' + e.target.id, '#Room-options')
+    displayOptions();
 });
 
-$('#Location-options :checkbox').click(function (e) {
+
+
+$('#Location-options :checkbox').click(function (eventObj) {
     $("#Location-options :checkbox").each(function(){
-        if ($(this).prop('id') != e.target.id) {
+        if ($(this).prop('id') != eventObj.target.id) {
             $(this).prop('checked', false);
+        } else {
+            locationchecked = $(this).prop('checked');
         }
     })
-    displayOptions('#' + e.target.id, '#Room-options');
+    displayOptions();
 });
     
-function displayOptions(id, nextid) {       
-    $(id).change(function() {
-        if (this.checked) {
-          $(nextid).show();
-        //   $('#otherField').attr('required', '');
-        //   $('#otherField').attr('data-error', 'This field is required.');
-        } else {
-          $(nextid).hide();
-        //   $('#otherField').removeAttr('required');
-        //   $('#otherField').removeAttr('data-error');
-        }
-    });
+function displayOptions() {
+    console.log(locationchecked + " type: "+ typechecked);
+    if (locationchecked || typechecked) {
+        $('#Room-options').show();
+    //   $('#otherField').attr('required', '');
+    //   $('#otherField').attr('data-error', 'This field is required.');
+    } else {
+        $('#Room-options').hide();
+    //   $('#otherField').removeAttr('required');
+    //   $('#otherField').removeAttr('data-error');
+    }
 }
 
-$("#Room-options").wrapAll("<div class='custom-checkbox'></div>");
+// $("#Room-options").wrapAll("<div class='custom-checkbox'></div>");
