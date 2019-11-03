d350505f70f3ec72b9cc7e81d7942ebd5ab62c31
Jason <22659287+Jason1im@users.noreply.github.com>
2019-11-03 17:40:29 +0800

    add logic to allow only 1 checkbox to be selected

--- resource.js
+++ resource.js
@@ -1,12 +1,38 @@
-$("#defaultUnchecked").change(function() {
-    if (this.checked) {
-      $('#Location-options').show();
-    //   $('#otherField').attr('required', '');
-    //   $('#otherField').attr('data-error', 'This field is required.');
-    } else {
-      $('#Location-options').hide();
-    //   $('#otherField').removeAttr('required');
-    //   $('#otherField').removeAttr('data-error');
-    }
+$('#type-checkbox-list :checkbox').click(function (e) {
+    $("#type-checkbox-list :checkbox").each(function(){
+        if ($(this).prop('id') != e.target.id) {
+            $(this).prop('checked', false);
+        }
+    })
+    displayOptions('#' + e.target.id)
 });
+
+
+
+
+    
+function displayOptions(id) {
+    $(id).change(function() {
+        if (this.checked) {
+          $('#Location-options').show();
+        //   $('#otherField').attr('required', '');
+        //   $('#otherField').attr('data-error', 'This field is required.');
+        } else {
+          $('#Location-options').hide();
+        //   $('#otherField').removeAttr('required');
+        //   $('#otherField').removeAttr('data-error');
+        }
+    });
+}
+// $("#defaultUnchecked").change(function() {
+//     if (this.checked) {
+//       $('#Location-options').show();
+//     //   $('#otherField').attr('required', '');
+//     //   $('#otherField').attr('data-error', 'This field is required.');
+//     } else {
+//       $('#Location-options').hide();
+//     //   $('#otherField').removeAttr('required');
+//     //   $('#otherField').removeAttr('data-error');
+//     }
+// });
 
