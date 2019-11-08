1b29b669253e11fee6bd73378cfe356b122a779f
Donatas <donatas.tamosauskas@gmail.com>
2019-11-08 17:03:56 +0800

    Datepicker fix for all trials

---
+++ push_to_heroku.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git subtree push --prefix Trial_1 heroku master &&
+git subtree push --prefix Trial_2 heroku2 master &&
+git subtree push --prefix Trial\ 3 heroku3 master &&
+git subtree push --prefix Trial\ 4 heroku4 master &&
+git subtree push --prefix Trial\ 5 heroku5 master &&
+git subtree push --prefix Trial\ 6 heroku6 master &&
+git subtree push --prefix Trial\ 7 heroku7 master &&
+git subtree push --prefix Trial\ 8 heroku8 master
+
