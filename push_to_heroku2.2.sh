c161d2218a39536abb9e1c3750f8ffa987bd29d5
Donatas <donatas.tamosauskas@gmail.com>
2019-11-13 22:00:08 +0800

    New script versions

---
+++ push_to_heroku2.2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git subtree push --prefix arrangement2.2/Trial_1 heroku master &&
+git subtree push --prefix arrangement2.2/Trial_2 heroku2 master &&
+git subtree push --prefix arrangement2.2/Trial\ 3 heroku3 master &&
+git subtree push --prefix arrangement2.2/Trial\ 4 heroku4 master &&
+git subtree push --prefix arrangement2.2/Trial\ 5 heroku5 master &&
+git subtree push --prefix arrangement2.2/Trial\ 6 heroku6 master &&
+git subtree push --prefix arrangement2.2/Trial\ 7 heroku7 master &&
+git subtree push --prefix arrangement2.2/Trial\ 8 heroku8 master
+
---
+++ push_to_heroku2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git subtree push --prefix arrangement2.1/Trial_1 heroku2-1 master &&
+git subtree push --prefix arrangement2.1/Trial_2 heroku2-2 master &&
+git subtree push --prefix arrangement2.1/Trial\ 3 heroku2-3 master &&
+git subtree push --prefix arrangement2.1/Trial\ 4 heroku2-4 master &&
+git subtree push --prefix arrangement2.1/Trial\ 5 heroku2-5 master &&
+git subtree push --prefix arrangement2.1/Trial\ 6 heroku2-6 master &&
+git subtree push --prefix arrangement2.1/Trial\ 7 heroku2-7 master &&
+git subtree push --prefix arrangement2.1/Trial\ 8 heroku2-8 master
+
